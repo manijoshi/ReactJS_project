224160ffc97cb4e8eddf152f3aa3ab516048fa18
manijoshi <65389710+manijoshi@users.noreply.github.com>
2024-02-24 21:21:50 +0000

    added contactcard component and inline styling.

---
+++ contact-app/src/components/ContactCard.js
@@ -0,0 +1,16 @@
+import React from "react";
+import user from "../images/user.png";
+const ContactCard=(props)=>{
+    var {id,name,email} = props.contact;
+    return(
+        <div className="item">
+            <img className="ui avtar image" style={{height:"50px", width:"80px"}} src={user} alt="user" />
+            <div className="content">
+                <div className="header">{name}</div>
+                <div>{email}</div>
+            </div>
+            <i style={{color:"red",marginTop:"7px"}} className="trash alternate outline icon"></i>
+        </div>
+    );
+};
+export default ContactCard;
